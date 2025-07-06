3d5960fa11a8e48804b223dde11db5e0f84a350e
Nikhil Dhanda <nikhildhanda84@gmail.com>
2025-07-06 13:59:17 +0530

    making deployment changes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 connectDb();
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5174',
+  origin: 'http://localhost:5174' || 'https://roadmapgenerator.vercel.app',
   credentials: true,
 }));
 app.use("/api/user", require('./routes/userRoute'))
@@ -18,7 +18,7 @@ app.use("/api/roadmap", require('./routes/roadmapRoute'))
 app.use(errorHandler)
 
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0',() => {
     console.log(`Server running on port ${port}`);
     
 })
